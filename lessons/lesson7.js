623d2d088028329736ba445226a2dc36ba2c1dc8
ihsan <ozgeihsan@gma>
2024-09-30 00:25:49 +0300

    loops updated

--- lessons/lesson7.js
+++ lessons/lesson7.js
@@ -1,36 +1,42 @@
 //loops
-console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
 
 // for(intial statement;condition; increment-decrement){
 
 // }
 
-for(let i=0; i<5;i++){
-    console.log("Hello World Loop")
+//for loop (for i loop)
+
+for (let i = 0; i < 5; i++) {
+  console.log("Hello World Loop");
 }
 
-var cars = ["Volvo","Toyota","Tesla"]
+var cars = ["Volvo", "Toyota", "Tesla"];
 
 // for of Loop
-for(let car of cars){
-    console.log(car)
+for (let car of cars) {
+  console.log(car);
 }
 
-for(let car of cars){
-    console.log(car)
-    if(car=="Toyota")
-        {
-            break
-        }
+for (let car of cars) {
+  console.log(car);
+  if (car == "Toyota") {
+    break;
+  }
 }
 
 console.log();
 //ES6 syntax
 
-cars.forEach(car =>{
-    console.log(car)
-})
+cars.forEach((car) => {
+  console.log(car);
+});
+console.log();
+
+cars.forEach((car) => console.log(car));
+
+console.log(cars.map((s) => s.toUpperCase()));
